388a07bb86953579a6e225fb50f5ca873a640953
frederick.ney <frederick.ney@gmail.com>
2017-02-16 15:50:45 +0100

    defining TODO for monitoring

---
+++ Monitoring.py
@@ -0,0 +1,14 @@
+__author__ = 'Frederick NEY'
+from labjack import ljm
+import LabjackT7Lib as Kernel
+
+
+def monitor_dio_ain(handle):
+    while(True):
+        """
+        #TODO wait timer
+        #TODO read register
+        #TODO print register content
+        #TODO continue execution while esc unpressed
+        """
+    return
