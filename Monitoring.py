a2a3726894181e27c1d9568bd6772facdcdb102e
frederick.ney <frederick.ney@gmail.com>
2017-02-18 16:02:20 +0100

    setting close of devices connection and testing if some reconnection thread is alive for killing them if alive

--- Monitoring.py
+++ Monitoring.py
@@ -18,22 +18,29 @@ def monitor_dio_ain(handles, information, display_sec=1, conn_attempt=5, conn_tr
     """
     Globals.add_global_handles(handles)
     Globals.add_global_information(information)
+    threads = []
     while not Globals.exiting:
         time.sleep(display_sec)
         for handle in Globals.handles:
             try:
+                print("getting information")
                 # TODO read register
                 # TODO print register content
             except ljm.ljm.LJMError:
                 index = Globals.handles.index(handle)
                 info = Globals.information[index]
-                print("Device " + str(info) + " disconnected")
+                print("Device " + str(info) + " is disconnected")
                 thread = Thread(info, conn_attempt, conn_try_sec)
+                threads.append(thread)
                 Globals.information.remove(Globals.information[index])
                 Globals.handles.remove(handle)
                 print("Device " + str(info) + " removed from list of devices")
                 print("Trying to reconnect to " + str(info))
                 thread.start()
-
+    for thread in threads:
+        if thread.is_alive():
+            thread.exit()
+    for handle in Globals.handles:
+        ljm.close(handle)
     return
 
