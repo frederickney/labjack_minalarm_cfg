48da5835f781feea5ec2fb226ba47ced352fc66e
frederick.ney <frederick.ney@gmail.com>
2017-02-18 15:28:45 +0100

    adding comments

--- threads.py
+++ threads.py
@@ -5,17 +5,31 @@ import time
 from labjack import ljm
 
 
-
 class Thread(threading.Thread):
+    """
+        object Thread used to create threads.
+    """
 
     def __init__(self, connection_info, reconnection_attempt, second):
+        """
+            constructor of the Thread object with some useful information
+        :param connection_info: it will be a list of len 3 with at index 0 the device type,
+                                index 1 the connection type and at index 2 the serial number of the device
+        :param reconnection_attempt: number of time it will try to reconnect to devices
+        :param second: number of second between reconnection
+        :return:
+        """
         self.connection_info = connection_info
         self.reconnection_attempt = reconnection_attempt
         self.second = second
         self.error = None
 
     def run(self):
+        """
+            main algorithm threads used to reconnect to devices after losing it connection
+        """
         for i in range(self.reconnection_attempt):
+            time.sleep(self.second)
             try:
                 handle = ljm.open(
                     self.connection_info[0],
@@ -29,7 +43,6 @@ class Thread(threading.Thread):
                     return 0
             except ljm.ljm.LJMError as LjmError:
                 self.error = LjmError._errorString
-            time.sleep(self.second)
         print(
             "\nUnable to reconnect to device " + str(self.connection_info)
             + " Error: " + LjmError._errorString
